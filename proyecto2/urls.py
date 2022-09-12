25a75171b035db67bd5c5fdedc19b04e50fdb475
Cris22 <cristhofersandoval4@gmail.com>
2022-09-12 15:54:13 -0300

    segunda rama creadaa

--- proyecto2/urls.py
+++ proyecto2/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path+
+from segundaApp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('p3/',views.terceravista),
+    path('p4/',views.cuartavista),
 ]
